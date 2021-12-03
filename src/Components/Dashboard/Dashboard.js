90149247f6bf36777d070c21affa6172f46ca998
Parjanya Modi <parjanya.cs19@bmsce.ac.in>
2021-12-03 16:11:01 +0530

    Updates

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,4 +1,41 @@
+import NavBar from "../NavBar/NavBar";
+import dash from "./dash.js";
 const Dashboard = (props) => {
-    return <p>Dashboard</p>;
-}
+  return (
+    <>
+      <NavBar />
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-1"></div>
+          <div className="col-lg-5 first-div-home">
+            <p>
+              <span className="home-details-title h2">Name : </span>
+              <span className="home-details-content">{dash.name}</span>
+            </p>
+            <p>
+              <span className="home-details-title">USN : </span>
+              <span className="home-details-content">{dash.usn}</span>
+            </p>
+            <p>
+              <span className="home-details-title">Email : </span>
+              <span className="home-details-content">{dash.email}</span>
+            </p>
+            <p>
+              <span className="home-details-title">Department : </span>
+              <span className="home-details-content">{dash.department}</span>
+            </p>
+            <p>
+              <span className="home-details-title">Gender : </span>
+              <span className="home-details-content">{dash.gender}</span>
+            </p>
+          </div>
+          <div className="col-lg-5">
+            <p className="home-name">Name :</p>
+          </div>
+          <div className="col-lg-1"></div>
+        </div>
+      </div>
+    </>
+  );
+};
 export default Dashboard;
---
+++ src/Components/Dashboard/dash.js
@@ -0,0 +1,15 @@
+const dash = {
+  sid: "12121212121212",
+  usn: "1BM19CS084",
+  name: "Matam Vijayeshjeevan",
+  email: "vijayeshjeevan.cs19@bmsce.ac.in",
+  department: "Computer Science",
+  gender: "Male",
+  phno: "+91 80509 78125",
+  semester: "5",
+  cgpa: "8.53",
+  img: "https://lh3.googleusercontent.com/fife/AAWUweUDs79zIeTfzBB1uveUiZLilD6aIBxIQ93vQJ9cBE4Pg0LyRJRR33LRzn5m5Ryy1E2Buz6xqbONxP9AUUgLp1_bB9w8zW2xWaAk8YtsHtlygCMcsYOpykAQjiHiSbs3TNtOkdqowUSlx80aeQSWOWavnLxqj7BMFRk6aFFX7gPfHgBEinsqV8zeIZi1g0M2a_ogTUHyZqi7utlRqblTtBWtXd1BIiOnL-l2aSCcx01eV5MdWS_R88pc6NLHWLAdbyOSGhDoK4Pdh1EQmeVgEpkSI7WWPf1OnNwuVyt_MZOJX5Ndj1eyvOG8uKg7mbL4tB_Gt5T8tO_AHvLtE7r6ndMvSlo3ePILXlboH0yVcv3x4LVgTDPX3yAh4Dh3upLBHTL61SNhfCyawuAXBaKfl6Wy29nMtcq9SVUvQ2dnDBBLc4rHyyjhmN58JlwMNk76Tjj751i6VAYcmYI92AY9ONkyBYxvwSCwOXCL62fqShm1bfybRssnxu9WEugLvxUOJTDAxOhHL-x5bjhcwUzkCjVDnsjgo8z2_CLD7ZeG_8VbNAQ8A5FE7sdnXSWXp6OeFYT0ND3uj6cuaVeB-vHn9Dbqz-KYfoaK1hKFx2dLej6utdy_qkGs2qcxA6h4e8iaYgZDIqxczULPEHxwbh1ftCJzuOpTWHpoqbsONt7lvGJ8eA5_t4KYDizwIPSzsJlb8g953dh7HkOoaKqKjKqz4GqLPCpMp_TpEsFCTm6s9zVWgA=s83-c",
+  proctor: "212121212121",
+};
+
+export default dash;
